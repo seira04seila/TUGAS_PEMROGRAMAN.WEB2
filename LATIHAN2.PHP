d53c5c57a1dda834a6d8632e12c8e0a000ab3999
seira04seila <148703655+seira04seila@users.noreply.github.com>
2023-10-23 20:31:58 +0700

    Add files via upload

---
+++ LATIHAN2.PHP
@@ -0,0 +1,19 @@
+<?php
+
+$x = 10;
+
+echo "Aku adalah angka $x. </br>";
+
+$HasilKali = $x * 8;
+echo "Jika aku dikali 8, jumlahku sekarang $HasilKali. </br>";
+
+$HasilBagi = $HasilKali / 4;
+echo "Jika aku dibagi 4, jumlahku sekarang $HasilBagi. </br>";
+
+$HasilKurang = $HasilBagi - 6;
+echo "Jika aku dikurang 6, jumlahku sekarang $HasilKurang. </br>";
+
+$HasilTambah = $HasilKurang + 2;
+echo "Jika aku ditambah 2, jumlahku sekarang $HasilTambah. </br>";
+
+?>
